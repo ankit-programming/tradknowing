86f36b07228f2b5f43cfb57e94dfd581fa9c3698
Ankit <70933302+ankit-programming@users.noreply.github.com>
2023-02-24 15:19:34 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,2 +1,27 @@
+'''while True:
+    exec(open("tv bot1.py").read())'''
+
+
+import tv_bot1
+import imaplib
+
+emailid = "tsignalmail@gmail.com"
+passward = "ztipkqtxgltshpqh"
+IMAP = 'imap.gmail.com'
+
+server_i = imaplib.IMAP4_SSL(IMAP)
+server_i.login(emailid, passward)
+
+server_i.select("inbox")
+
+typ, data_i = server_i.search(None, 'FROM "noreply@tradingview.com"') #Filter by sender
+
+for num in data_i[0].split():
+    #deleting the mails
+    server_i.store(num, '+FLAGS', r'(\Deleted)')
+    server_i.expunge()
+    print("E-mail deleted")
+
 while True:
-    exec(open("tv bot1.py").read())
+    tv_bot1.place_order()
+
